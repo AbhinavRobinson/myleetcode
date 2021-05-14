0973b1715fa198c07051ae3f3c8640773480ba29
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-05-14 11:29:09 +0530

    (feat): a simpler solution for q744

--- easy/744. Find Smallest Letter Greater Than Target.py
+++ easy/744. Find Smallest Letter Greater Than Target.py
@@ -1,6 +1,6 @@
 '''
-Runtime: 108 ms, faster than 58.17% of Python3 online submissions for Find Smallest Letter Greater Than Target.
-Memory Usage: 14.3 MB, less than 82.97% of Python3 online submissions for Find Smallest Letter Greater Than Target.
+Runtime: 104 ms, faster than 81.51% of Python3 online submissions for Find Smallest Letter Greater Than Target.
+Memory Usage: 14.6 MB, less than 37.16% of Python3 online submissions for Find Smallest Letter Greater Than Target.
 '''
 
 from typing import List
@@ -8,14 +8,15 @@ from typing import List
 
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        # Wrap Around
         if target >= letters[-1]:
             return letters[0]
 
         l, r = 0, len(letters)-1
         while l < r:
-            m = (l + r)//2
-            if letters[m] <= target:
-                l = m + 1
-            else:
+            m = (l+r)//2
+            if letters[m] > target:
                 r = m
-        return letters[l % len(letters)]
+            else:
+                l = m + 1
+        return letters[l]
