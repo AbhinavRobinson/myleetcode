bd7b0c08aecdc2feef5ae9672b872466c13b852e
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-05-26 20:20:30 +0530

    (feat): add q48

---
+++ medium/48. Rotate Image.cpp
@@ -0,0 +1,31 @@
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Rotate Image.
+// Memory Usage: 7.1 MB, less than 70.60% of C++ online submissions for Rotate Image.
+
+#include <vector>;
+
+using namespace std;
+
+class Solution
+{
+public:
+  void rotate(vector<vector<int>> &matrix)
+  {
+    int n = matrix.size();
+    int a = 0;
+    int b = n - 1;
+    while (a < b)
+    {
+      for (int i = 0; i < (b - a); ++i)
+      {
+        swap(matrix[a][a + i], matrix[a + i][b]);
+        swap(matrix[a][a + i], matrix[b][b - i]);
+        swap(matrix[a][a + i], matrix[b - i][a]);
+      }
+      ++a;
+      --b;
+    }
+  }
+};
